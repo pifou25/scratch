2575ce7660d411babfb7d446ec9bec0e22f17f6b
Pifou25 <nicolas_gras@hotmail.com>
2024-01-21 15:37:00 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -2,9 +2,10 @@ const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
 
-class Scratch3YourExtension {
+class Scratch3JeedomExtension {
 
     constructor (runtime) {
+        this.runtime = runtime;
         // put any setup for your extension here
     }
 
@@ -13,76 +14,47 @@ class Scratch3YourExtension {
      */
     getInfo () {
         return {
-            // unique ID for your extension
-            id: 'yourScratchExtension',
-
-            // name that will be displayed in the Scratch UI
-            name: 'Demo',
-
-            // colours to use for your extension blocks
-            color1: '#000099',
-            color2: '#660066',
-
-            // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
-            // your Scratch blocks
-            blocks: [
-                {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
-
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
-
-                    // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
-
-                    // arguments used in the block
-                    arguments: {
-                        MY_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 123,
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        MY_STRING: {
-                            // default value before the user sets something
-                            defaultValue: 'hello',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.STRING
-                        }
-                    }
+          // unique ID for your extension
+          id: 'jeedom',
+      
+          // name displayed in the Scratch UI
+          name: 'jeedom',
+      
+          // colours to use for your extension blocks
+          color1: '#454449',
+          color2: '#94CA02',
+      
+          // your Scratch blocks
+          blocks: [
+            {
+              // function where your code logic lives
+              opcode: 'callJeedomApi',
+      
+              // type of block
+              blockType: BlockType.REPORTER,
+      
+              // label to display on the block
+              text: 'Call Jeedom API [METHOD, PARAMS]',
+      
+              // arguments used in the block
+              arguments: {
+                METHOD: {
+                  // name of API method
+                  defaultValue: "ping",
+      
+                  // type/shape of the parameter
+                  type: ArgumentType.STRING
+                },
+                PARAMS: {
+                    // json list of parameters
+                    defaultValue: '{"apikey": "abc123", "datetime": "0"}',
+        
+                    // type/shape of the parameter
+                    type: ArgumentType.STRING
+                  }
                 }
-            ]
+            }
+          ]
         };
     }
 
@@ -90,11 +62,30 @@ class Scratch3YourExtension {
     /**
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
+     * implementation of Jeedom Json RPC API
+     * https://doc.jeedom.com/fr_FR/core/4.4/jsonrpc_api
      */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
-    }
+    callJeedomApi ({ METHOD, PARAMS }) {
+        return fetch('https://192.168.1.25/core/api/jeeApi.php', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json, text/plain, */*',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(PARAMS)
+          })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            else {
+              return { title: 'Unknown' };
+            }
+          })
+          .then((bookinfo) => {
+            return bookinfo.title;
+          });
+      }
 }
 
-module.exports = Scratch3YourExtension;
+module.exports = Scratch3JeedomExtension;
